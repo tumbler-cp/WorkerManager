50131361019cb9ab41161cc886e6418a9b58fe9c
arahnik <abdujalol04@mail.ru>
2024-12-19 00:17:41 +0300

    fixes

--- src/main/java/lab/arahnik/entity/User.java
+++ src/main/java/lab/arahnik/entity/User.java
@@ -23,6 +23,7 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String username;
     private String password;
     @Enumerated(EnumType.STRING)
--- src/main/java/lab/arahnik/service/JwtService.java
+++ src/main/java/lab/arahnik/service/JwtService.java
@@ -4,7 +4,6 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -71,5 +70,4 @@ public class JwtService {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
     }
-
 }
